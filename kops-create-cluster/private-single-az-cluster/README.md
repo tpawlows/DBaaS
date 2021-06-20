c0478811f273f12257bd638998b8cbe88303e4ae
Tomasz Pawlowski <trpawlows@gmail.com>
2021-06-20 18:22:21 +0200

    add readme to private-single-az-cluster

---
+++ kops-create-cluster/private-single-az-cluster/README.md
@@ -0,0 +1,12 @@
+# private-singls-az-cluster
+
+### Usage
+To create cluster with default values (desired by me) just run script.
+```bash
+./private-single-az-cluster.sh
+```
+It will create a k8s cluster using kOps name **k8s.retipuj.com** in hosted zone **retipuj.com**.
+### Schema
+```diff
+- TODO: ADD SCHEMA/PICTURE
+```
--- kops-create-cluster/private-single-az-cluster/private-single-az-cluster.sh
+++ kops-create-cluster/private-single-az-cluster/private-single-az-cluster.sh
@@ -3,7 +3,7 @@
 # cluster_name hosted_zone availability_zones
 echo "Cluster name:			${1:-k8s.retipuj.com}"
 echo "Hosted Zone:			${2:-retipuj.com}"
-echo "Availability Zones:	${3:-eu-north-1a}"
+echo "Availability Zones:		${3:-eu-north-1a}"
 
 # Create and save configuration to s3
 kops create cluster \
@@ -18,8 +18,8 @@ kops create cluster \
 	--networking=kube-router 
 echo "Cluster configuration saved to: $KOPS_STATE_STORE"
 
-# # Create actual cluster on AWS
+# Create actual cluster on AWS
 kops update cluster ${1:-k8s.retipuj.com} --yes --admin
 
-# # Wait for kOps to create cluster (about 12 min) 
+# Wait for kOps to create cluster (about 12 min) 
 kops validate cluster --wait 15m
